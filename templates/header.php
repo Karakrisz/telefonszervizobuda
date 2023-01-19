70a06f7885fbdffb26a9ed6a433494cfb2cb1c4d
Boaz <webevolution13@gmail.com>
2023-01-19 14:05:29 +0100

    External code ( front - end )

---
+++ templates/header.php
@@ -0,0 +1,20 @@
+<div class="custom-cursor__cursor"></div>
+<div class="custom-cursor__cursor-two"></div>
+
+<!-- style switcher -->
+<div class="style-switcher">
+    <a href="#" id="switcher-toggler"><i class="fa fa-cog"></i></a>
+    <h3>Layout Options</h3>
+    <div class="layout-feature" id="colorMode">
+        <a href="" class="dark-switcher" data-theme="ambed-dark">Dark</a>
+        <a href="" class="light-switcher" data-theme="ambed-light">Light</a>
+        <button class="boxed-switcher">Boxed</button><!-- /.ltr-switcher -->
+    </div><!-- /.language-feature -->
+</div>
+<!-- end style switcher -->
+
+
+
+<div class="preloader">
+    <div class="preloader__image"></div>
+</div>
